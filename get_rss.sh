66703c8074b5e198ad50096bd7d78856746366a9
Adam DJ Brett <adamdjbrett@users.noreply.github.com>
2024-06-11 17:20:51 -0500

    removing firehose

--- get_rss.sh
+++
@@ -1,5 +0,0 @@
-wget -O feed.xml 'https://input.relcfp.com/feed.xml'
-sed -i 's/<!\[CDATA\[//g' feed.xml
-sed -i 's/]]>//g' feed.xml
-sed -i 's/<br>/<br\/>/g' feed.xml
-sed -i 's/<hr>/<hr\/>/g' feed.xml
